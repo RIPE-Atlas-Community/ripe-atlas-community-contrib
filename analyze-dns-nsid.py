55de1c856fae9fcd2cbafcd5c54cf800e4d27fac
ioc32 <inigo@infornografia.net>
2013-08-05 14:41:16 +0200

    fetch measurement results using files or references to data API

--- analyze-dns-nsid.py
+++ analyze-dns-nsid.py
@@ -2,7 +2,8 @@
 
 """ Python code to analyze RIPE Atlas UDM (User-Defined Measurements)
 results. This one is for DNS NSID queries (find the identity of a name
-server). It uses a RIPE Atlas JSON file as input.
+server). It uses a RIPE Atlas JSON file as input or a reference to a
+public measurement.
 
 Stephane Bortzmeyer <bortzmeyer@nic.fr>
 """
@@ -11,6 +12,8 @@ import json
 import sys
 import base64
 import collections
+import urllib
+import getopt
 
 # DNS Python http://www.dnspython.org/
 import dns.message
@@ -22,10 +25,23 @@ class Items:
 def by_num(l, r):
     return -cmp(ns_names[l].num, ns_names[r].num)
 
-# Measurement #1008591 for instance
-filename = sys.argv[1]
+# Try measurement #1008591 for instance
 
-results = json.loads(open(filename).read())
+try:
+    optlist, args = getopt.getopt (sys.argv[1:], "f:d:", ["file=","data-api="])
+    for option, value in optlist:
+        if option == "--file" or option == "-f":
+            results = json.loads(open(value).read())
+        elif option == "--data-api" or option == "-d":
+            url = "https://udm01.atlas.ripe.net/api/v1/measurement/" + value + "/result/"
+            results = json.load(urllib.urlopen(url))
+        else:
+            usage("Unknown option %s" % option)
+            sys.exit(1)
+
+except getopt.error, reason:
+    usage(reason)
+    sys.exit(1)
 
 net_failures = 0
 nsid_failures = 0
