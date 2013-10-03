RIPE-Atlas-Community/ripe-atlas-community-contrib

commit b24501a67e27661277cde7dedcf6378cc219a8e5

Answer the questions in the right order.

.Tutorial/exercice-ping-1009150.py
@@ -14,7 +14,7 @@ results = json.loads(file.read())
 
 
 # Main loop over the results
-unreachable = 0
+unreachable = set()
 for probe in results:
     # Per-test loop
     probe_ok = False
@@ -22,12 +22,12 @@ for probe in results:
         if test.has_key('rtt'):
             probe_ok = True
             break
-    if not probe_ok:
-        unreachable += 1
-        print "Probe %s has a problem" % probe['prb_id']
-if unreachable == 0:
+        unreachable.add(probe['prb_id'])
+if len(unreachable) == 0:
     print "All probes were reachable"
-elif unreachable == 1:
+elif len(unreachable) == 1:
     print "One probe was unreachable"
-elif unreachable > 1:
-    print "%d probes were unreachable" % unreachable
+else:
+    print "%d probes were unreachable" % len(unreachable)
+for prb_id in unreachable:
+    print "Probe %s has a problem" % prb_id

commit 348a85869fdc90e9c11b07c0fe58247116053eb8

Fix bug introduced in commit b24501a67e27661277cde7dedcf6378cc219a8e5. Check probe_ok before adding probe to the unreachable set.

.Tutorial/exercice-ping-1009150.py
@@ -22,6 +22,7 @@ for probe in results:
         if test.has_key('rtt'):
             probe_ok = True
             break
+    if not probe_ok:
         unreachable.add(probe['prb_id'])
 if len(unreachable) == 0:
     print "All probes were reachable"