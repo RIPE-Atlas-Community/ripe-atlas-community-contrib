9d7013b948aaa49b6dd002b39810f477966457d1
Kim Minh Kaplan <kimminh.kaplan+commit@afnic.fr>
2013-10-03 08:23:05 +0000

    Solution in Common Lisp to the tutorial exercice on measurement 1009150.
    Requires a Common Lisp (tested with SBCL) and CL-JSON.
    
    SBCL
    http://www.sbcl.org/
    Steel Bank Common Lisp (SBCL) is a high performance Common Lisp compiler.
    
    Quicklisp
    http://www.quicklisp.org/
    Quicklisp makes it easy to get started with a rich set of
    community-developed Common Lisp libraries
    
    CL-JSON
    http://common-lisp.net/project/cl-json/
    CL-JSON provides an encoder of Lisp objects to JSON format and a
    corresponding decoder of JSON data to Lisp objects.
    
    Installing SBCL, Quicklisp and CL-JSON on Debian:
    
        sudo aptitude install sbcl cl-quicklisp
        sbcl --load /usr/share/cl-quicklisp/quicklisp.lisp \
    	 --eval '(quicklisp-quickstart:install)' \
    	 --eval '(ql:quickload "cl-json")' \
    	 --quit
    
    Running with SBCL and Quicklisp:
    
        sbcl --load ~/quicklisp/setup.lisp \
             --script exercice-ping-1009150-lisp.lisp

---
+++ .Tutorial/exercice-ping-1009150-lisp.lisp
@@ -0,0 +1,24 @@
+;; How many probes did not reach the target, even once?
+;; Which are they? (field prb_id)
+
+;; Data is in https://atlas.ripe.net/api/v1/measurement/1009150/result/
+
+(require "cl-json")
+
+(defun access (object member)
+  (cdr (assoc member object)))
+
+(let* ((results (with-open-file (s "1009150.json")
+		  (json:decode-json s)))
+       (probes (loop for probe in results
+		  unless (loop for test in (access probe :result)
+			    thereis (access test :rtt))
+		  collect probe))
+       (unreachable (length probes)))
+  (cond
+    ((= 1 unreachable)
+     (format t "One probe was unreachable~%"))
+    ((< 1 unreachable)
+     (format t "~D probes were unreachable~%" unreachable)))
+  (loop for probe in probes
+     do (format t "Probe ~S has a problem~%" (access probe :prb--id))))
