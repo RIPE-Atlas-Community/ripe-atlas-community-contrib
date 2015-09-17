14397fbad3f066de63a5e4bd4523d98a462ff205
Stephane Bortzmeyer <stephane+github@bortzmeyer.org>
2015-09-17 15:11:22 +0200

    Ability to test with several nameservers (reusing measurement's set of probes)

--- resolve-name.py
+++ resolve-name.py
@@ -72,7 +72,7 @@ def usage(msg=None):
     --probetouse or -u N : uses only this probe
     --old_measurement MSMID or -g MSMID : uses the probes of measurement #MSMID
     --measurement-ID=N or -m N : do not start a measurement, just analyze a former one
-    --nameserver=IPaddr or -e IPaddr : query this name server (default is to query the probe's resolver)
+    --nameserver=IPaddr[,...] or -e IPaddr : query this name server (default is to query the probe's resolver)
     """ % (qtype, requested)
     
 try:
@@ -102,6 +102,7 @@ try:
             probe_to_use = value
         elif option == "--nameserver" or option == "-e":
             nameserver = value
+            nameservers = string.split(nameserver, ",")
         elif option == "--help" or option == "-h":
             usage()
             sys.exit(0)
@@ -113,6 +114,7 @@ try:
             only_one_per_probe = False
         elif option == "--ipv6" or option == "-6":
             ip_family = 6
+            # TODO: when using option --nameserver, set it automatically depending on the address family?
         elif option == "--verbose" or option == "-v":
             verbose = True
         elif option == "--machinereadable" or option == "-b":
@@ -180,6 +182,28 @@ if measurement_id is None:
             if area is not None or country is not None or asn is not None:
                 usage("Specify country *or* area *or* ASn *or* old measurement")
                 sys.exit(1)
+        else:
+            data["probes"][0]["type"] = "area"
+            data["probes"][0]["value"] = "WW"
+    if verbose and machine_readable:
+        usage("Specify verbose *or* machine-readable output")
+        sys.exit(1)
+    if (display_probes or display_resolvers) and machine_readable:
+        usage("Display probes/resolvers *or* machine-readable output")
+        sys.exit(1)
+
+    if nameserver is None:
+        nameservers = [None,]
+        
+    for nameserver in nameservers:
+        if nameserver is None:
+            data["definitions"][0]["use_probe_resolver"] = True
+        else:
+            data["definitions"][0]["use_probe_resolver"] = False
+            data["definitions"][0]["target"] = nameserver
+            data["definitions"][0]["description"] += (" via nameserver %s" % nameserver)
+
+        if old_measurement is not None:
             data["probes"][0]["requested"] = 500 # Dummy value, anyway,
                                                     # but necessary to get
                                                     # all the probes
@@ -187,137 +211,129 @@ if measurement_id is None:
             data["probes"][0]["type"] = "msm"
             data["probes"][0]["value"] = old_measurement
             data["definitions"][0]["description"] += (" from probes of measurement #%s" % old_measurement)
+
+        if measurement_id is None:
+            if verbose:
+                print data
+            measurement = RIPEAtlas.Measurement(data,
+                                            lambda delay: sys.stderr.write(
+                    "Sleeping %i seconds...\n" % delay))
+
+            if not machine_readable:
+                print "Measurement #%s for %s/%s uses %i probes" % \
+                (measurement.id, domainname, qtype, measurement.num_probes)
+
+            old_measurement = measurement.id
+            results = measurement.results(wait=True)
         else:
-            data["probes"][0]["type"] = "area"
-            data["probes"][0]["value"] = "WW"
-    if nameserver is None:
-        data["definitions"][0]["use_probe_resolver"] = True
-    else:
-        data["definitions"][0]["use_probe_resolver"] = False
-        data["definitions"][0]["target"] = nameserver
-    if verbose and machine_readable:
-        usage("Specify verbose *or* machine-readable output")
-        sys.exit(1)
-    if (display_probes or display_resolvers) and machine_readable:
-        usage("Display probes/resolvers *or* machine-readable output")
-        sys.exit(1)
-        
-    if verbose:
-        print data
-        
-    measurement = RIPEAtlas.Measurement(data,
-                                        lambda delay: sys.stderr.write(
-            "Sleeping %i seconds...\n" % delay))
+            measurement = RIPEAtlas.Measurement(data=None, id=measurement_id)
+            results = measurement.results(wait=False)
 
-    if not machine_readable:
-        print "Measurement #%s for %s/%s uses %i probes" % \
-        (measurement.id, domainname, qtype, measurement.num_probes)
+        probes = 0
+        successes = 0
 
-    measurement_id = measurement.id
-    results = measurement.results(wait=True)
-else:
-    measurement = RIPEAtlas.Measurement(data=None, id=measurement_id)
-    results = measurement.results(wait=False)
+        qtype_num = dns.rdatatype.from_text(qtype) # Raises dns.rdatatype.UnknownRdatatype if unknown
+        sets = collections.defaultdict(Set)
+        if display_probes:
+            probes_sets = collections.defaultdict(Set)
+        if display_resolvers:
+            resolvers_sets = collections.defaultdict(Set)
+        for result in results:
+            probes += 1
+            probe_id = result["prb_id"]
+            if result.has_key("resultset"):
+                for result_i in result['resultset']:
+                    if result_i.has_key("result"):
+                        try:
+                            resolver = str(result_i['dst_addr'])
+                            answer = result_i['result']['abuf'] + "=="
+                            content = base64.b64decode(answer)
+                            msg = dns.message.from_wire(content)
+                            successes += 1
+                            myset = []
+                            if msg.rcode() == dns.rcode.NOERROR:
+                                for rrset in msg.answer:
+                                    for rdata in rrset:
+                                        if rdata.rdtype == qtype_num:
+                                            myset.append(string.lower(str(rdata)))
+                            else:
+                                myset.append("ERROR: %s" % dns.rcode.to_text(msg.rcode()))
+                            myset.sort()
+                            set_str = " ".join(myset)
+                            sets[set_str].total += 1
+                            if display_probes:
+                                if probes_sets.has_key(set_str):
+                                    probes_sets[set_str].append(probe_id)
+                                else:
+                                    probes_sets[set_str] = [probe_id,]
+                            if display_resolvers:
+                                if resolvers_sets.has_key(set_str):
+                                    if not (resolver in resolvers_sets[set_str]):
+                                        resolvers_sets[set_str].append(resolver)
+                                else:
+                                    resolvers_sets[set_str] = [resolver,]
+                        except dns.message.TrailingJunk:
+                            if not machine_readable:
+                                print "Probe %s failed (trailing junk)" % result['prb_id']
+                    if only_one_per_probe:
+                        break
+            elif result.has_key("result"):
+                    try:
+                        resolver = str(result['dst_addr'])
+                        answer = result['result']['abuf'] + "=="
+                        content = base64.b64decode(answer)
+                        msg = dns.message.from_wire(content)
+                        successes += 1
+                        myset = []
+                        if msg.rcode() == dns.rcode.NOERROR:
+                            for rrset in msg.answer:
+                                for rdata in rrset:
+                                    if rdata.rdtype == qtype_num:
+                                        myset.append(string.lower(str(rdata)))
+                        else:
+                            myset.append("ERROR: %s" % dns.rcode.to_text(msg.rcode()))
+                        myset.sort()
+                        set_str = " ".join(myset)
+                        sets[set_str].total += 1
+                        if display_probes:
+                                if probes_sets.has_key(set_str):
+                                    probes_sets[set_str].append(probe_id)
+                                else:
+                                    probes_sets[set_str] = [probe_id,]
+                        if display_resolvers:
+                                if resolvers_sets.has_key(set_str):
+                                    if not (resolver in resolvers_sets[set_str]):
+                                        resolvers_sets[set_str].append(resolver)
+                                else:
+                                    resolvers_sets[set_str] = [resolver,]
+                    except dns.message.TrailingJunk:
+                        if not machine_readable:
+                            print "Probe %s failed (trailing junk)" % result['prb_id']
+        if sort:
+            sets_data = sorted(sets, key=lambda s: sets[s].total, reverse=True)
+        else:
+            sets_data = sets
+        details = []
+        if not machine_readable and nameserver is not None:
+                print "Nameserver %s" % nameserver
+        for myset in sets_data:
+            detail = ""
+            if display_probes:
+                detail = "(probes %s)" % probes_sets[myset]
+            if display_resolvers:
+                detail += "(resolvers %s)" % resolvers_sets[myset]
+            if not machine_readable:
+                print "[%s] : %i occurrences %s" % (myset, sets[myset].total, detail)
+            else:
+                details.append("[%s];%i" % (myset, sets[myset].total))
 
-probes = 0
-successes = 0
-
-qtype_num = dns.rdatatype.from_text(qtype) # Raises dns.rdatatype.UnknownRdatatype if unknown
-sets = collections.defaultdict(Set)
-if display_probes:
-    probes_sets = collections.defaultdict(Set)
-if display_resolvers:
-    resolvers_sets = collections.defaultdict(Set)
-for result in results:
-    probes += 1
-    probe_id = result["prb_id"]
-    if result.has_key("resultset"):
-        for result_i in result['resultset']:
-            if result_i.has_key("result"):
-                try:
-                    resolver = str(result_i['dst_addr'])
-                    answer = result_i['result']['abuf'] + "=="
-                    content = base64.b64decode(answer)
-                    msg = dns.message.from_wire(content)
-                    successes += 1
-                    myset = []
-                    if msg.rcode() == dns.rcode.NOERROR:
-                        for rrset in msg.answer:
-                            for rdata in rrset:
-                                if rdata.rdtype == qtype_num:
-                                    myset.append(string.lower(str(rdata)))
-                    else:
-                        myset.append("ERROR: %s" % dns.rcode.to_text(msg.rcode()))
-                    myset.sort()
-                    set_str = " ".join(myset)
-                    sets[set_str].total += 1
-                    if display_probes:
-                        if probes_sets.has_key(set_str):
-                            probes_sets[set_str].append(probe_id)
-                        else:
-                            probes_sets[set_str] = [probe_id,]
-                    if display_resolvers:
-                        if resolvers_sets.has_key(set_str):
-                            if not (resolver in resolvers_sets[set_str]):
-                                resolvers_sets[set_str].append(resolver)
-                        else:
-                            resolvers_sets[set_str] = [resolver,]
-                except dns.message.TrailingJunk:
-                    if not machine_readable:
-                        print "Probe %s failed (trailing junk)" % result['prb_id']
-            if only_one_per_probe:
-                break
-    elif result.has_key("result"):
-            try:
-                resolver = str(result['dst_addr'])
-                answer = result['result']['abuf'] + "=="
-                content = base64.b64decode(answer)
-                msg = dns.message.from_wire(content)
-                successes += 1
-                myset = []
-                if msg.rcode() == dns.rcode.NOERROR:
-                    for rrset in msg.answer:
-                        for rdata in rrset:
-                            if rdata.rdtype == qtype_num:
-                                myset.append(string.lower(str(rdata)))
-                else:
-                    myset.append("ERROR: %s" % dns.rcode.to_text(msg.rcode()))
-                myset.sort()
-                set_str = " ".join(myset)
-                sets[set_str].total += 1
-                if display_probes:
-                        if probes_sets.has_key(set_str):
-                            probes_sets[set_str].append(probe_id)
-                        else:
-                            probes_sets[set_str] = [probe_id,]
-                if display_resolvers:
-                        if resolvers_sets.has_key(set_str):
-                            if not (resolver in resolvers_sets[set_str]):
-                                resolvers_sets[set_str].append(resolver)
-                        else:
-                            resolvers_sets[set_str] = [resolver,]
-            except dns.message.TrailingJunk:
-                if not machine_readable:
-                    print "Probe %s failed (trailing junk)" % result['prb_id']
-if sort:
-    sets_data = sorted(sets, key=lambda s: sets[s].total, reverse=True)
-else:
-    sets_data = sets
-details = []
-for myset in sets_data:
-    detail = ""
-    if display_probes:
-        detail = "(probes %s)" % probes_sets[myset]
-    if display_resolvers:
-        detail += "(resolvers %s)" % resolvers_sets[myset]
-    if not machine_readable:
-        print "[%s] : %i occurrences %s" % (myset, sets[myset].total, detail)
-    else:
-        details.append("[%s];%i" % (myset, sets[myset].total))
-            
-if not machine_readable:
-    print ("Test done at %s" % time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime()))
-    print ""
-else:
-    print ",".join([domainname, qtype, str(measurement.id), "%s/%s" % (len(results), measurement.num_probes), \
-                    time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())] + details)
+        if not machine_readable:
+            print ("Test done at %s" % time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime()))
+            print ""
+        else:
+            if nameserver is None:
+                ns = "DEFAULT RESOLVER"
+            else:
+                ns = nameserver
+            print ",".join([domainname, qtype, str(measurement.id), "%s/%s" % (len(results), measurement.num_probes), \
+                            time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime()), ns] + details)
