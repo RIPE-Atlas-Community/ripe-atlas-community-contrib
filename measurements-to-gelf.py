a320ec08681ceebf3246cfdf28fcf7668a401bab
awlx <awlnx@penguinfriends.org>
2016-04-12 11:05:13 +0200

    Replaced tabs with 4 spaces

--- measurements-to-gelf.py
+++ measurements-to-gelf.py
@@ -22,14 +22,14 @@ api_key = '<api_key>'
 
 # Parse Args
 if len(sys.argv) == 1:
-	print "# Prototype of a RIPE Atlas to GELF parser"
-	print "#"
-	print "# Usage: python measurements-to-gelf.py <measurement id> <timeframe in min>"
-	print "#"
-	print "# Example: python measurements-to-gelf.py 12323 5"
-	print "#"
-	print "# Please define Server and Port inside the script and you need an API Key for Geolocation from https://geocoder.opencagedata.com/pricing"
-	exit(1) 
+    print "# Prototype of a RIPE Atlas to GELF parser"
+    print "#"
+    print "# Usage: python measurements-to-gelf.py <measurement id> <timeframe in min>"
+    print "#"
+    print "# Example: python measurements-to-gelf.py 12323 5"
+    print "#"
+    print "# Please define Server and Port inside the script and you need an API Key for Geolocation from https://geocoder.opencagedata.com/pricing"
+    exit(1) 
 
 measurement_id = str(sys.argv[1])
 timeframe_secs = int(sys.argv[2]) * 60
@@ -44,11 +44,11 @@ def getplace(lat, lon):
     components = j['results'][0]['components']
     country = town = None
     try:
-    	country = components['country']
-    	state = components['state']
+        country = components['country']
+        state = components['state']
     except Exception as e:
-	country = "N/A"
-	state = "N/A"
+        country = "N/A"
+        state = "N/A"
     return state, country
 
 
@@ -60,37 +60,37 @@ measurement_url = "https://atlas.ripe.net/api/v2/measurements/%s/results?start=%
 print measurement_url
 
 try:
-	d = requests.get(measurement_url)
+    d = requests.get(measurement_url)
 except Exception as e:
-        print e
-	exit(1)
+    print e
+    exit(1)
 
 data = d.json()
 
 gelf = UdpClient(gelf_server, port=gelf_port)
 for probe in data:
-	print probe['prb_id']
-	log = {}
-	v = requests.get('https://atlas.ripe.net/api/v1/probe/' + str(probe['prb_id']) + '/')
-	details = v.json()
-	location = getplace(details['latitude'], details['longitude'])
-	log['short_message'] = 'RIPE Atlas Data of Probe ' + str(probe['prb_id'])
-	log['host'] = 'ripe-atlas'
-	log['level'] = syslog.LOG_INFO
-	log['timestamp'] = probe['timestamp']
-	log['_ripe_atlas_prbid'] = probe['prb_id']
-	log['_ripe_atlas_dst_addr'] = probe['dst_addr']
-	log['_ripe_atlas_src_addr'] = probe['from']
-	log['_ripe_atlas_type'] = probe['type']
-	log['_ripe_atlas_proto'] = probe['proto']
-	log['_ripe_atlas_sent_pkts'] = probe['sent']
-	log['_ripe_atlas_country'] = details['country_code']
-	log['_ripe_atlas_location'] = location[0] + ',' + location[1]
-	log['_ripe_atlas_rcvd_pkts'] = probe['rcvd']
-	log['_ripe_atlas_avg_rtt'] = probe['avg']
-	log['_ripe_atlas_max_rtt'] = probe['max']
-	log['_ripe_atlas_min_rtt'] = probe['min']
-	gelf.log(log)
+    print probe['prb_id']
+    log = {}
+    v = requests.get('https://atlas.ripe.net/api/v1/probe/' + str(probe['prb_id']) + '/')
+    details = v.json()
+    location = getplace(details['latitude'], details['longitude'])
+    log['short_message'] = 'RIPE Atlas Data of Probe ' + str(probe['prb_id'])
+    log['host'] = 'ripe-atlas'
+    log['level'] = syslog.LOG_INFO
+    log['timestamp'] = probe['timestamp']
+    log['_ripe_atlas_prbid'] = probe['prb_id']
+    log['_ripe_atlas_dst_addr'] = probe['dst_addr']
+    log['_ripe_atlas_src_addr'] = probe['from']
+    log['_ripe_atlas_type'] = probe['type']
+    log['_ripe_atlas_proto'] = probe['proto']
+    log['_ripe_atlas_sent_pkts'] = probe['sent']
+    log['_ripe_atlas_country'] = details['country_code']
+    log['_ripe_atlas_location'] = location[0] + ',' + location[1]
+    log['_ripe_atlas_rcvd_pkts'] = probe['rcvd']
+    log['_ripe_atlas_avg_rtt'] = probe['avg']
+    log['_ripe_atlas_max_rtt'] = probe['max']
+    log['_ripe_atlas_min_rtt'] = probe['min']
+    gelf.log(log)
 
 
 
